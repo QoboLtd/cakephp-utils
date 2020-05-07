c2fe355573cdc20cc0db813e0001f47fb7888c6d
Iacovos Constantinou <softius@gmail.com>
2020-05-07 16:00:34 +0300

    Add cacheConfig option (task #14655)

--- config/module_config.php
+++ config/module_config.php
@@ -23,6 +23,7 @@ return [
             dirname(__DIR__),
             'src', 'ModuleConfig', 'Parser', 'Schema',
         ]),
+        'cacheConfig' => 'default',
         'classMapVersion' => 'V3',
         'classMap' => [
             'V3' => [
